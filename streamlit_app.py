f765597fbd2af3ccb6018b39741c0326b3944acc
Kanz385 <145867422+Kanz385@users.noreply.github.com>
2023-09-25 12:46:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,30 +68,24 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-# Load your data
-df = pd.read_csv("https://raw.githubusercontent.com/Kanz385/inter/main/PCOS_data.csv")  # Adjust the path to your CSV file
+# Corrected URL (assuming that the file extension should be .csv)
+df = pd.read_csv("https://raw.githubusercontent.com/Kanz385/inter/main/PCOS_data.csv")
 
-# Streamlit app function
 def main():
     st.title("Interactive Bar Chart")
 
     # Sidebar for user input
     st.sidebar.header("Select Data for Bar Chart")
 
-    # Create selectboxes to choose columns for x and y axes
     x_column = st.sidebar.selectbox("X-Axis Data (Category)", df.columns)
     y_column = st.sidebar.selectbox("Y-Axis Data (Value)", df.columns)
-
-    # Add a selectbox for highlighting a specific category
     highlight_category = st.sidebar.selectbox("Highlight Category (Optional)", ['None'] + df[x_column].unique().tolist())
-
-    # Filter the data based on the selected category to highlight
+    
     if highlight_category != 'None':
         filtered_data = df[df[x_column] == highlight_category]
     else:
         filtered_data = df
 
-    # Create the bar chart using Plotly
     fig = go.Figure()
     fig.add_trace(go.Bar(x=filtered_data[x_column], y=filtered_data[y_column], marker_color='blue'))
     fig.update_layout(title=f"{x_column} vs {y_column}", xaxis_title=x_column, yaxis_title=y_column)
@@ -99,6 +93,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 
